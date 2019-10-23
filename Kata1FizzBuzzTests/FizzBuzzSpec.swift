2a29682db5270dd7a2325352dd6f5963255a980a
FordPass Dev <fordpass@ford.com>
2019-10-23 11:18:03 -0400

    add first test for int number

--- Kata1FizzBuzzTests/FizzBuzzSpec.swift
+++ Kata1FizzBuzzTests/FizzBuzzSpec.swift
@@ -9,11 +9,23 @@
 import Quick
 import Nimble
 
+class FizzBuzz {
+    let number: Int = 1
+}
+
 class FizzBuzzSpec: QuickSpec {
     override func spec() {
-
         describe("Fizz Buzz") {
-        }
+            // arrange
+            let subject = FizzBuzz()
 
+            context("the number is one") {
+                it("should print 1") {
+                    // act
+                    // assert
+                    expect(subject.number) == 1
+                }
+            }
+        }
     }
 }
