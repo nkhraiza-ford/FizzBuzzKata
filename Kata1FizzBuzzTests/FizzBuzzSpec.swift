dba713a9bc7d970c304a2a1d1926caac3a611991
FordPass Dev <fordpass@ford.com>
2019-10-23 15:29:27 -0400

    add fizz buzz class with test covering initial case

--- Kata1FizzBuzzTests/FizzBuzzSpec.swift
+++ Kata1FizzBuzzTests/FizzBuzzSpec.swift
@@ -9,10 +9,21 @@
 import Quick
 import Nimble
 
+class FizzBuzz {
+   static func calculate(_ value:Int) -> Int {
+        return 1
+    }
+}
+
 class FizzBuzzSpec: QuickSpec {
     override func spec() {
         describe("Fizz Buzz") {
             // arrange
+            context("1") {
+                it("produces output of 1"){
+                    expect(FizzBuzz.calculate(1)) == 1
+                }
+            }
             // act
             // assert
         }
