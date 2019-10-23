9910b0ed701eef09f64535ca9a0c42168f51d538
FordPass Dev <fordpass@ford.com>
2019-10-23 15:36:54 -0400

    handled normal case

--- Kata1FizzBuzzTests/FizzBuzzSpec.swift
+++ Kata1FizzBuzzTests/FizzBuzzSpec.swift
@@ -10,22 +10,24 @@ import Quick
 import Nimble
 
 class FizzBuzz {
-   static func calculate(_ value:Int) -> Int {
-        return 1
+   static func calculate(_ value: Int) -> Int {
+        return value
     }
 }
 
 class FizzBuzzSpec: QuickSpec {
     override func spec() {
         describe("Fizz Buzz") {
-            // arrange
             context("1") {
                 it("produces output of 1"){
                     expect(FizzBuzz.calculate(1)) == 1
                 }
             }
-            // act
-            // assert
+            context("2") {
+                it("should produce an output of 2") {
+                    expect(FizzBuzz.calculate(2)) == 2
+                }
+            }
         }
     }
 }
