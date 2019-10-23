279d1f4f584951338a307d52c72fe9607da44a91
FordPass Dev <fordpass@ford.com>
2019-10-23 10:34:48 -0400

    Added first spec

---
+++ Kata1FizzBuzzTests/FizzBuzzSpec.swift
@@ -0,0 +1,19 @@
+//
+//  FizzBuzzSpec.swift
+//  Kata1FizzBuzz
+//
+//  Created by Khraizat, Nader (N.M.) on 10/23/19.
+//  Copyright © 2019 Khraizat, Nader (N.M.). All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class FizzBuzzSpec: QuickSpec {
+    override func spec() {
+
+        describe("Fizz Buzz") {
+        }
+
+    }
+}
